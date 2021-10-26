00b4f010b82482dc7e9ab44d6b44419cbaf39262
Dendy Saputra <dendyscik@gmail.com>
2021-10-26 12:59:58 +0700

    Menambahkan file /c/Users/acer/Praktikum1/Tugaslatihan.py

---
+++ Tugaslatihan.py
@@ -0,0 +1,14 @@
+print " Biodata "
+# Mengambil Input
+Nama Lengkap = input("Dendy Saputra:")
+Nama Panggilan = input("Dendy:")
+NPM = input("312110201:")
+Tempat Lahir = input("Bekasi:")
+Tanggal Lahir = input("21 Juli 2003:")
+Telepon = input("085880028306:")
+Alamat = input("Jl.Cisadane ID, Graha Mattel blok N2/31:")
+
+# Menampilkan Output
+print("Assalamu'alaikum.")
+print("Let me introduce my self. My name is {Dendy Saputra}, but you can call me {Dendy}. My NPM is {312110201}. Iwas born in {Bekasi} and I am {18} years old. I am very glad if you want to invite my house in {Jl.Cisadane ID, Graha Mattel blok N2/31}. So, don't forget to call me before with the number {085880028306}")
+print("Thank you.")
